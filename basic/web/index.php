abe149bd07c597cfe3d439c662b1144d196b60ca
annajeane <42904301+annajeane@users.noreply.github.com>
2018-11-05 12:52:46 +0700

    file
    basic

---
+++ basic/web/index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Portal Informatika</title>
+      <link rel="stylesheet" href="css/components.css">
+      <link rel="stylesheet" href="css/responsee.css">
+      <link rel="stylesheet" href="css/icons.css">
+      <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+      <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+      <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+      <!-- CUSTOM STYLE -->
+      <link rel="stylesheet" href="css/template-style.css">
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+      <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+      <script type="text/javascript" src="js/jquery-ui.min.js"></script>    
+      <script type="text/javascript" src="js/template-scripts.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+
+   </head>
+   <body class="size-1140">
+
+      <header>
+         <div id="topbar">
+            <div class="line">
+               <div class="s-12 m-6 l-6">
+                  <p>KONTAK INFO: <strong>XXX-XXX-XXX</strong> | <strong>umn.ac.id/test</strong></p>
+               </div>
+               <div class="s-12 m-6 l-6">
+                  <div class="social right">
+                     <a><i class="icon-facebook_circle"></i></a> <a><i class="icon-twitter_circle"></i></a> <a><i class="icon-google_plus_circle"></i></a> <a><i class="icon-instagram_circle"></i></a>
+                  </div>
+               </div>
+            </div>  
+         </div> 
+         <nav>
+            <div class="line">
+               <div class="s-12 l-3 ">
+                  <img src="img/logo.png" style="width: 100%;padding-top: 15px;">
+               </div>
+               <div class="top-nav s-12 l-9" id="cssmenu">
+                  <p class="nav-text">NAVIGASI</p>
+                  <ul class="right">
+                     <li class="active-item"><a href="index.php">Beranda</a></li>
+                     <li class='has-sub'><a href='tentang.php'><span>Tentang</span></a>
+                        <ul>
+                          <li><a href='#section-one'><span>Sejarah</span></a></li>
+                           <li><a href='#section-two'><span>Visi Misi</span></a></li>
+                          <li class='has-sub'><a href='#'><span>Kompetensi</span></a>
+                             <ul>
+                                <li><a href='#a'><span>Kompetensi Unggulan</span></a></li>
+                                <li class='last'><a href='#a'><span>Kompetensi Tambahan</span></a></li>
+                             </ul>
+                          </li>
+                          <li><a href='#a'><span>Akreditasi</span></a></li>
+                          <li><a href='#a'><span>Mitra</span></a></li>
+                        </ul>
+                      </li>
+                      <li class='has-sub'><a href='#a'><span>Kurikulum</span></a>
+                        <ul>
+                          <li><a href='#a'><span>Mata Kuliah</span></a></li>
+                          <li class='has-sub'><a href='#a'><span>Peminatan</span></a>
+                             <ul>
+                                <li><a href='#a'><span>Game Development</span></a></li>
+                                <li><a href='#a'><span>Cyber Security</span></a></li>
+                                <li><a href='#a'><span>Oracle</span></a></li>
+                                <li><a href='#a'><span>CISCO</span></a></li>
+                                <li class='last'><a href='#a'><span>SAP</span></a></li>
+                             </ul>
+                          </li>
+                          <li><a href='#a'><span>Program Internasional</span></a></li>
+                        </ul>
+                      </li>
+                      <li class='has-sub'><a href='dosen.php'><span>Dosen</span></a>
+                        <ul>
+                          <li><a href='#a'><span>List Dosen</span></a></li>                          
+                          <li><a href='#a'><span>Detail Dosen</span></a></li>
+                        </ul>
+                      </li>
+                      <li class='has-sub'><a href='#a'><span>Berita & Acara</span></a>
+                        <ul>
+                          <li><a href='#a'><span>List Berita</span></a></li>                          
+                          <li><a href='#a'><span>List Acara</span></a></li>
+                        </ul>
+                      </li>
+                     <li><a href="#a">Fasilitas</a></li>
+                     
+                  </ul>
+               </div>
+            </div>
+         </nav>
+
+      </header>  
+
+         <div id="carousel">
+            <div id="owl-demo" class="owl-carousel owl-theme"> 
+               <div class="item">
+                  <img src="img/first.jpg" alt="">
+                  <div class="line"> 
+                     <div class="text hide-s">
+                        <div class="line"> 
+                          <div class="prev-arrow hide-s hide-m">
+                             <i class="icon-chevron_left"></i>
+                          </div>
+                          <div class="next-arrow hide-s hide-m">
+                             <i class="icon-chevron_right"></i>
+                          </div>
+                        </div> 
+                        <h2>Berita Terkini 1</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="item">
+                  <img src="img/second.jpg" alt="">
+                  <div class="line">
+                     <div class="text hide-s"> 
+                        <div class="line"> 
+                          <div class="prev-arrow hide-s hide-m">
+                             <i class="icon-chevron_left"></i>
+                          </div>
+                          <div class="next-arrow hide-s hide-m">
+                             <i class="icon-chevron_right"></i>
+                          </div>
+                        </div> 
+                        <h2>Berita Terkini 2</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="item">
+                  <img src="img/third.jpg" alt="">
+                  <div class="line">
+                     <div class="text hide-s">
+                        <div class="line"> 
+                          <div class="prev-arrow hide-s hide-m">
+                             <i class="icon-chevron_left"></i>
+                          </div>
+                          <div class="next-arrow hide-s hide-m">
+                             <i class="icon-chevron_right"></i>
+                          </div>
+                        </div> 
+                        <h2>Berita Terkini 3</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- FIRST BLOCK -->
+         <div id="first-block">
+            <div class="line">
+               <h1>Prodi Informatika UMN</h1>
+               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+               <div class="s-12 m-4 l-2 center"><a class="white-btn" href="#more">Selengkapnya</a></div>
+            </div>
+         </div>
+         <!-- FEATURES -->
+         <div id="features">
+            <div class="line">
+               <div class="margin">
+                  <div class="s-12 m-6 l-3 margin-bottom">
+                     <i class="icon-star icon3x"></i>
+                     <h2>Keunggulan</h2>
+                     <p>Selain kemampuan dan keahlian teknis, lulusan Program Studi Informatika UMN juga dibekali dengan keahlian untuk menjadi peneliti muda dan wirausaha yang mampu memanfaatkan kemajuan Teknologi Informasi dan Komunikasi.</p>
+                  </div>
+                  <div class="s-12 m-6 l-3 margin-bottom">
+                     <i class="icon-isight icon3x"></i>
+                     <h2>Prospek Karir</h2>
+                     <p>Desktop Application Programmer, Web Designer, Web Programmer, Database Designer, Database Developer, Android Application Developer, Mobile Cross-platform Application Developer, dst.</p>
+                  </div>
+                  <div class="s-12 m-6 l-3 margin-bottom">
+                     <i class="icon-heart icon3x"></i>
+                     <h2>Program Internasional</h2>
+                     <p>UMN juga memiliki Program Internasional Dual Degree di bidang Computer Science bekerjasama dengan Universitas Teknologi Swinburne,  Melbourne, Australia (Informasi lebih detail).</p>
+                  </div>
+                  <div class="s-12 m-6 l-3 margin-bottom">
+                     <i class="icon-tablet icon3x"></i>
+                     <h2>Persyaratan Masuk</h2>
+                     <p>Lulusan SMA jurusan IPA<br>Lulusan SMK jurusan Teknik Komputer, Teknik Informatika, Teknik Komputer dan Jaringan.</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- ABOUT US -->
+
+         <div id="about-us">
+            <div class="s-12 m-12 l-6 media-container">
+               <img src="img/about.jpg" alt="">
+            </div>
+            <article class="s-12 m-12 l-6">
+               <h2>"xxx<br> xxx<br> xxxx"</h2>
+
+               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet 
+                 dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit 
+                 lobortis nisl ut aliquip ex ea commodo consequat.
+               </p>
+               <div class="about-us-icons">
+                  <i class="icon-paperplane_ico"></i> <i class="icon-trophy"></i> <i class="icon-clock"></i>
+               </div>
+
+            </article>
+         </div>
+         <!-- SERVICES -->
+         <div id="services">
+            <div class="line">
+               <div class="margin">
+                 <div class="s-12 m-6 l-6 margin-bottom">
+
+                     <h2><strong>VISI</strong></h2>
+                     <p>Menjadi Program Studi Strata Satu Informatika unggulan yang menghasilkan lulusan berwawasan internasional yang kompeten di bidang ilmu komputer (computer science), berjiwa wirausaha dan berbudi pekerti luhur.</p>
+                  </div>
+                  <div class="s-12 m-6 l-6 margin-bottom">
+ 
+                     <h2><strong>MISI</strong></h2>
+                     <p>1. Menyelenggarakan pembelajaran dengan teknologi dan kurikulum terbaik serta didukung tenaga pengajar profesional.<br>2. Melaksanakan kegiatan penelitian di bidang Informatika untuk memajukan ilmu dan teknologi Informatika.<br>3. Melaksanakan kegiatan pengabdian kepada masyarakat berbasis ilmu dan teknologi Informatika dalam rangka mengamalkan ilmu dan teknologi Informatika.</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+       
+      <!-- FOOTER -->
+      <footer>
+         <div class="line">
+            <div class="s-12 l-6">
+               <p>© 2018 Universitas Multimedia Nusantara.</p>
+            </div>
+            <div class="s-12 l-6">
+               <a class="right" href="http://www.umn.ac.id.com" title="UMN"><strong>Universitas Multimedia Nusantara
+                </strong><br>
+                <br>Jl. Scientia Boulevard, Gading Serpong, 
+                <br>Tangerang, Banten-15811 Indonesia
+                <br>(T)+62-21.5422.0808
+                <br>(F)+62-21.5422.0800
+                <br>(T)0811 9555 510
+                <br>(T)0811 9555 501
+                <br>e-mail: marketing@umn.ac.id</a>
+            </div>
+         </div>
+      </footer>
+      <script type="text/javascript" src="js/responsee.js"></script>
+      <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>
+      <script type="text/javascript">
+         jQuery(document).ready(function($) {
+            var theme_slider = $("#owl-demo");
+            var owl = $('#owl-demo');
+            owl.owlCarousel({
+              nav: false,
+              dots: true,
+              items: 1,
+              loop: true,
+              autoplay: true,
+              autoplayTimeout: 6000
+            });
+            var owl = $('#owl-demo2');
+            owl.owlCarousel({
+              nav: true,
+              dots: false,
+              items: 1,
+              loop: true,
+              navText: ["&#xf007","&#xf006"],
+              autoplay: true,
+              autoplayTimeout: 4000
+            });
+        
+            // Custom Navigation Events
+            $(".next-arrow").click(function() {
+                theme_slider.trigger('next.owl');
+            })
+            $(".prev-arrow").click(function() {
+                theme_slider.trigger('prev.owl');
+            })     
+        }); 
+      </script>
+   </body>
+</html>
---
+++ basic/web/tentang.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Portal Informatika</title>
+      <link rel="stylesheet" href="css/components.css">
+      <link rel="stylesheet" href="css/responsee.css">
+      <link rel="stylesheet" href="css/icons.css">
+      <link rel="stylesheet" href="owl-carousel/owl.carousel.css">
+      <link rel="stylesheet" href="owl-carousel/owl.theme.css">
+      <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+      <!-- CUSTOM STYLE -->
+      <link rel="stylesheet" href="css/template-style.css">
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+      <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+      <script type="text/javascript" src="js/jquery-ui.min.js"></script>    
+      <script type="text/javascript" src="js/template-scripts.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+
+   </head>
+   <body class="size-1140">
+      <header>
+         <div id="topbar">
+            <div class="line">
+               <div class="s-12 m-6 l-6">
+                  <p>KONTAK INFO: <strong>XXX-XXX-XXX</strong> | <strong>umn.ac.id/test</strong></p>
+               </div>
+               <div class="s-12 m-6 l-6">
+                  <div class="social right">
+                     <a><i class="icon-facebook_circle"></i></a> <a><i class="icon-twitter_circle"></i></a> <a><i class="icon-google_plus_circle"></i></a> <a><i class="icon-instagram_circle"></i></a>
+                  </div>
+               </div>
+            </div>  
+         </div> 
+         <nav>
+            <div class="line">
+                   <div class="s-12 l-3 ">
+                  <img src="img/logo.png" style="width: 100%;padding-top: 15px;">
+               </div>
+               <div class="top-nav s-12 l-9" id="cssmenu">
+                  <p class="nav-text">NAVIGASI</p>
+                  <ul class="right">
+                     <li><a href="index.php">Beranda</a></li>
+                     <li class='has-sub'><a href='tentang.php'><span>Tentang</span></a>
+                        <ul>
+                         
+                          <li><a href='#section-one'><span>Sejarah</span></a></li>
+                           <li><a href='#section-two'><span>Visi Misi</span></a></li>
+                          <li class='has-sub'><a href='#section-three'><span>Kompetensi</span></a>
+                             <ul>
+                                <li><a href='#a'><span>Kompetensi Unggulan</span></a></li>
+                                <li class='last'><a href='#a'><span>Kompetensi Tambahan</span></a></li>
+                             </ul>
+                          </li>
+                          <li><a href='#section-four'><span>Akreditasi</span></a></li>
+                          <li><a href='#section-five'><span>Mitra</span></a></li>
+                        </ul>
+                      </li>
+                      <li class='has-sub'><a href='#a'><span>Kurikulum</span></a>
+                        <ul>
+                          <li><a href='#a'><span>Mata Kuliah</span></a></li>
+                          <li class='has-sub'><a href='#a'><span>Peminatan</span></a>
+                             <ul>
+                                <li><a href='#a'><span>Game Development</span></a></li>
+                                <li><a href='#a'><span>Cyber Security</span></a></li>
+                                <li><a href='#a'><span>Oracle</span></a></li>
+                                <li><a href='#a'><span>CISCO</span></a></li>
+                                <li class='last'><a href='#a'><span>SAP</span></a></li>
+                             </ul>
+                          </li>
+                          <li><a href='#a'><span>Program Internasional</span></a></li>
+                        </ul>
+                      </li>
+                      <li class='has-sub'><a href='dosen.php'><span>Dosen</span></a>
+                        <ul>
+                          <li><a href='#a'><span>List Dosen</span></a></li>                          
+                          <li><a href='#a'><span>Detail Dosen</span></a></li>
+                        </ul>
+                      </li>
+                      <li class='has-sub'><a href='#a'><span>Berita & Acara</span></a>
+                        <ul>
+                          <li><a href='#a'><span>List Berita</span></a></li>                          
+                          <li><a href='#a'><span>List Acara</span></a></li>
+                        </ul>
+                      </li>
+                     <li><a href="#a">Fasilitas</a></li>
+                     
+                  </ul>
+               </div>
+            </div>
+         </nav>
+         <div id="section-menu" style="background-color: transparent;">
+          <ul>
+          </ul>
+         </div>
+      </header>  
+         <div id="latest-news">
+            <div class="line">
+              <h2 class="section-title">Sejarah</h2> 
+              <div class="margin">
+                  
+                 <section id="section-one" class="section scroll-item" title="Sejarah">
+                 <div class="inner">
+                   <h2>Sejarah</h2>
+                 </div>
+               </section>
+               </div>
+            </div>
+         </div>
+         <div id="services">
+            <div class="line">
+               <h2 class="section-title">VISI-MISI</h2>
+               <div class="margin">
+                <section id="section-two" class="section scroll-item" title="Visi-Misi">
+
+                </section>
+               </div>
+            </div>
+         </div>
+         <div id="latest-news">
+            <div class="line">
+              <h2 class="section-title">Kompetensi</h2> 
+              <div class="margin">
+                <section id="section-three" class="section scroll-item" title="Kompetensi">
+
+                </section>
+              </div>  
+            </div>
+         </div> 
+
+
+                  <div id="services">
+            <div class="line">
+               <h2 class="section-title">Akreditasi</h2>
+               <div class="margin">
+                <section id="section-four" class="section scroll-item" title="Akreditasi">
+
+                </section>
+               </div>
+            </div>
+         </div>
+         <div id="latest-news">
+            <div class="line">
+              <h2 class="section-title">Mitra</h2> 
+              <div class="margin">
+                <section id="section-five" class="section scroll-item" title="Mitra">
+
+                </section>
+              </div>  
+            </div>
+         </div> 
+        <!-- FOOTER -->
+      <footer>
+         <div class="line">
+            <div class="s-12 l-6">
+               <p>© 2018 Universitas Multimedia Nusantara.</p>
+            </div>
+            <div class="s-12 l-6">
+               <a class="right" href="http://www.umn.ac.id.com" title="UMN"><strong>Universitas Multimedia Nusantara
+                </strong><br>
+                <br>Jl. Scientia Boulevard, Gading Serpong, 
+                <br>Tangerang, Banten-15811 Indonesia
+                <br>(T)+62-21.5422.0808
+                <br>(F)+62-21.5422.0800
+                <br>(T)0811 9555 510
+                <br>(T)0811 9555 501
+                <br>e-mail: marketing@umn.ac.id</a>
+            </div>
+         </div>
+      </footer>
+      <script type="text/javascript" src="js/responsee.js"></script>
+      <script type="text/javascript" src="owl-carousel/owl.carousel.js"></script>
+      <script type="text/javascript">
+         jQuery(document).ready(function($) {
+            var theme_slider = $("#owl-demo");
+            var owl = $('#owl-demo');
+            owl.owlCarousel({
+              nav: false,
+              dots: true,
+              items: 1,
+              loop: true,
+              autoplay: true,
+              autoplayTimeout: 6000
+            });
+            var owl = $('#owl-demo2');
+            owl.owlCarousel({
+              nav: true,
+              dots: false,
+              items: 1,
+              loop: true,
+              navText: ["&#xf007","&#xf006"],
+              autoplay: true,
+              autoplayTimeout: 4000
+            });
+        
+            // Custom Navigation Events
+            $(".next-arrow").click(function() {
+                theme_slider.trigger('next.owl');
+            })
+            $(".prev-arrow").click(function() {
+                theme_slider.trigger('prev.owl');
+            })     
+        }); 
+         
+
+      </script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+<script>jQuery(function($) {
+
+  var html = $('html');
+  var viewport = $(window);
+  var viewportHeight = viewport.height();
+
+
+  var scrollMenu = $('#section-menu');
+  var timeout = null;
+
+  function menuFreeze() {
+    if (timeout !== null) {
+      scrollMenu.removeClass('freeze');
+      clearTimeout(timeout);
+    }
+    
+    timeout = setTimeout(function() {
+
+    }, 2000);
+  }
+  scrollMenu.mouseover(menuFreeze);
+
+
+  var sectionsHeight = {}, viewportheight, i = 0;
+  var scrollItem = $('.scroll-item');
+  var bannerHeight;
+
+  function sectionListen() {
+    viewportHeight = viewport.height();
+    bannerHeight = (viewportHeight);
+    $('.section').addClass('resize').css('height', bannerHeight);
+    scrollItem.each(function(){
+      sectionsHeight[this.title] = $(this).offset().top;
+    });
+  }
+  sectionListen();
+  viewport.resize(sectionListen);
+  viewport.bind('orientationchange', function() {
+    sectionListen();
+  });
+  
+  var count = 0;
+
+  scrollItem.each(function(){
+    var anchor = $(this).attr('id');
+    var title = $(this).attr('title');
+    count ++;
+    $('#section-menu ul').append('<li><a id="nav_' + title + '" href="#' + anchor + '"><span>' + count + '</span> ' + title + '</a></li>');
+  });
+  function menuListen() {
+    var pos = $(this).scrollTop();
+    pos = pos + viewportHeight * 0.625;
+    for(i in sectionsHeight){
+      if(sectionsHeight[i] < pos) {
+        $('#section-menu a').removeClass('active');
+        $('#section-menu a#nav_' + i).addClass('active');;
+        var newHash = '#' + $('.scroll-item[title="' + i + '"]').attr('id');
+        if(history.pushState) {
+            history.pushState(null, null, newHash);
+        } else {
+            location.hash = newHash;
+        }
+      } else {
+        $('#section-menu a#nav_' + i).removeClass('active');
+        if (pos < viewportHeight - 72) {
+          history.pushState(null, null, ' ');
+        }
+      }
+    }
+  }
+  scrollMenu.css('margin-top', scrollMenu.height() / 2 * -1);
+
+  scrollMenu.find('a').click(function() {
+    var href = $.attr(this, 'href');
+    $('html').animate({
+      scrollTop: $(href).offset().top
+    }, 500, function() {
+      window.location.hash = href;
+    });
+    return false;
+  });  
+
+  function scrollHandler() {
+    menuListen();
+    menuFreeze();
+  }
+  scrollHandler();
+  viewport.on('scroll', function() {
+    scrollHandler();
+
+  });
+});
+</script>
+
+   </body>
+</html>
